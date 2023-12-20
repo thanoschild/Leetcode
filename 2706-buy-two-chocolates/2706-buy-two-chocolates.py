faf57b2c1c18c0eb43dda3234721f66ca6aa9cd5
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2023-12-20 23:05:32 +0530

    Time: 48 ms (13.75%), Space: 13.3 MB (58.13%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def buyChoco(self, prices, money):
+        first_min_cost = second_min_cost = float("inf")
+
+        for p in prices:
+            if p < first_min_cost:
+                second_min_cost, first_min_cost = first_min_cost, p
+            else:
+                second_min_cost = min(second_min_cost, p)
+
+        leftover = money - (first_min_cost + second_min_cost)
+
+        return leftover if leftover >= 0 else money
+        
