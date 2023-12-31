c7b1068ff3264c9f55092f01ed30c221915037de
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2023-12-31 19:58:58 +0530

    Time: 19 ms (20.00%), Space: 9.7 MB (80.00%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
     int maximumLength(string s) {
-        map<pair<char, int>, int> mp;
+        map<string, int> mp;
         int n = s.size();
 
+        
         for(int i = 0; i<n; i++) {
-            int count = 1;
-            mp[{s[i], count}]++;
-            for(int j = i; j<n - 1; j++) {
+            string curr = "";
+            curr += s[i];
+            mp[curr]++;
+          
+            for(int j = i; j<n-1; j++) {
                 if(s[j] == s[j+1]) {
-                    count++;
-                    mp[{s[i], count}]++;
+                    curr += s[j];
+                    mp[curr]++;
                 }
                 else break;
             }
         }
-
+        
         int ans = 0;
         for(auto it : mp) {
             if(it.second >= 3) {
-                ans = max(ans, it.first.second);
+                ans = max(ans, (int)it.first.size());
             }
         }
-
+        
         return ans == 0 ? -1 : ans;
     }
 };
