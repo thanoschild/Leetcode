67809a1a095a2ac6068eee7069194c4bedc03a8b
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2023-12-31 20:06:08 +0530

    Time: 42 ms (100.00%), Space: 45.7 MB (50.00%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.java
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.java
@@ -1,33 +1,32 @@
 class Solution {
     public int maximumLength(String s) {
-        HashMap<Pair<Character, Integer>, Integer> map = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
         int n = s.length();
-
+        
         for(int i = 0; i<n; i++) {
-            int count = 1;
+            String curr = "";
             char c = s.charAt(i);
-            Pair<Character, Integer> pair = new Pair(c, count);
-            map.put(pair, map.getOrDefault(pair, 0) + 1);
-
+            curr += c;
+            map.put(curr, map.getOrDefault(curr, 0) + 1);
+            
             for(int j = i; j<n-1; j++) {
                 char a = s.charAt(j);
                 char b = s.charAt(j+1);
                 if(a == b) {
-                    count++;
-                    Pair<Character, Integer> curr = new Pair(c, count);
+                    curr += a;
                     map.put(curr, map.getOrDefault(curr, 0) + 1);
                 }
                 else break;
             }
         }
-
+        
         int ans = 0;
-        for(Map.Entry<Pair<Character, Integer>, Integer> entry : map.entrySet()) {
+        for(Map.Entry<String, Integer> entry : map.entrySet()) {
             if(entry.getValue() >= 3) {
-               ans = Math.max(ans, entry.getKey().getValue());
+                ans = Math.max(ans, entry.getKey().length());
             }
         }
-
+        
         return ans == 0 ? -1 : ans;
     }
 }
