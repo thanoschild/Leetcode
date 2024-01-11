9cc93f5b7c5c28e131e0e3c0b28eb0db5818e19c
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2024-01-11 13:07:29 +0530

    Time: 15 ms (72.62%), Space: 11.7 MB (95.78%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        vector<int> ans;
+        if(!root) return ans;
+
+        stack<Node*> st;
+        st.push(root);
+
+        while(!st.empty()) {
+            Node* t = st.top();
+            st.pop();
+
+            for(auto it : t->children) st.push(it);
+            ans.push_back(t->val);
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
