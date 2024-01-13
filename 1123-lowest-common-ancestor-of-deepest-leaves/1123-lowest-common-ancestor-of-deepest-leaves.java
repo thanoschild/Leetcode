dae28dc53f1336f2e07e11c9b6eff498bb9cd4fb
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2024-01-13 12:11:46 +0530

    Time: 1 ms (43.23%), Space: 44.5 MB (6.58%) - LeetHub

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Pair {
+    TreeNode node;
+    int depth;
+    
+    Pair(TreeNode node, int depth) {
+        this.node = node;
+        this.depth = depth;
+    }
+}
+
+class Solution {
+    
+    private Pair dfs(TreeNode root) {
+        if(root == null) return new Pair(null, 0);
+
+        Pair left = dfs(root.left);
+        Pair right = dfs(root.right);
+
+        if(left.depth > right.depth) return new Pair(left.node, left.depth + 1);
+        if(right.depth > left.depth) return new Pair(right.node, right.depth + 1);
+
+        return new Pair(root, left.depth + 1);
+    }
+
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        Pair p = dfs(root);
+        return p.node;
+    }
+}
