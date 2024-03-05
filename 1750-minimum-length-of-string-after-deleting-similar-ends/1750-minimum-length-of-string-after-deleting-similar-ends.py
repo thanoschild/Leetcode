4d7303ad3688ca11fda16223a9d7e29abed04065
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2024-03-05 22:35:15 +0530

    Time: 63 ms (83.33%), Space: 12.8 MB (100.00%) - LeetHub

---
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def minimumLength(self, s):
+        i, j = 0, len(s)-1
+        while i < j and s[i] == s[j]:
+            c = s[i]
+            while i <= j and s[i] == c: i += 1
+            while i < j and s[j] == c: j -= 1
+        return j - i + 1        
