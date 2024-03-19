1926ecb4f385a7533aaef41b9e4e2ca5cb1cd85e
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2024-03-19 22:52:41 +0530

    Time: 21 ms (50.03%), Space: 46.3 MB (9.81%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,18 +1,17 @@
-class Solution(object):
-    def leastInterval(self, tasks, n):
-        mp = {}
-        max_count = 0
-        for x in tasks:
-            if x in mp:
-                mp[x] += 1
-            else:
-                mp[x] = 1
-            max_count = max(max_count, mp[x])
-        
-        ans = (max_count - 1) * (n + 1)
-        for key, value in mp.items():
-            if value == max_count:
-                ans += 1
-        
-        return max(ans, len(tasks))
-        
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        HashMap<Character, Integer> mp = new HashMap<>();
+        int maxCount = 0;
+        for(char c : tasks) {
+            mp.put(c, mp.getOrDefault(c, 0) + 1);
+            maxCount = Math.max(maxCount, mp.get(c));
+        }
+
+        int ans = (maxCount - 1) * (n + 1);
+        for(int x : mp.values()) {
+            if(x == maxCount) ans++;
+        }
+
+        return Math.max(ans, tasks.length);
+    }
+}
