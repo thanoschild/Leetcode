925d27ae9db0016a85b5a66ec7e48fdb1b081aa1
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2024-04-10 17:06:32 +0530

    Time: 4 ms (86.41%), Space: 43.4 MB (65.78%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -1,14 +1,17 @@
-class Solution(object):
-    def deckRevealedIncreasing(self, deck):
-        n = len(deck)
-        deck.sort()
-        pq = collections.deque(range(n))
-        ans = [0] * n
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        int n = deck.length;
+        Arrays.sort(deck);
+        Queue<Integer> pq = new LinkedList<>();
+        int[] ans = new int[n];
 
-        for i in range(n):
-            ans[pq.popleft()] = deck[i]
-            if pq:
-                pq.append(pq.popleft())
+        for(int i = 0; i<n; i++) pq.add(i);
 
-        return ans
-        
+        for(int i = 0; i<n; i++) {
+            ans[pq.poll()] = deck[i];
+            pq.add(pq.poll());
+        }
+
+        return ans;
+    }
+}
