334d3f808b7fbfa101524b661777a59617d86af6
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2024-04-13 18:50:21 +0530

    Time: 11 ms (91.93%), Space: 11.5 MB (94.71%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,14 @@
+class Solution(object):
+    def sortColors(self, nums):
+        left, mid, right = 0, 0, len(nums) - 1
+        while mid <= right:
+            if nums[mid] == 0:
+                nums[left], nums[mid] = nums[mid], nums[left]
+                left += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid], nums[right] = nums[right], nums[mid]
+                right -= 1
+        
