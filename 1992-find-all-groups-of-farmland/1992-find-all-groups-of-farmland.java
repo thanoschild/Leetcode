92f135f070ded5110bdbee07d629d2bdde3cf050
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2024-04-20 12:43:50 +0530

    Time: 18 ms (28.90%), Space: 74.6 MB (6.46%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
@@ -1,29 +1,40 @@
-class Solution(object):
-    def dfs(self, grid, i, j, n, m, curr):
-        if i < 0 or i >= n or j < 0 or j >= m or grid[i][j] == 0:
-            return
-        
-        grid[i][j] = 0
-        curr[2] = max(i, curr[2])
-        curr[3] = max(j, curr[3])
-        dir = [-1, 0, 1, 0, -1]
+class Solution {
+    int [] dir = {-1, 0, 1, 0, -1};
+    private void dfs(int[][] grid, int i, int j, int n, int m, int[] curr) {
+        if(i < 0 || i >= n || j < 0 || j >= m || grid[i][j] == 0) return;
 
-        for k in range(4):
-            x, y = dir[k] + i, dir[k+1] + j
-            self.dfs(grid, x, y, n, m, curr)
-  
-    def findFarmland(self, grid):
-        ans = []
-        n, m = len(grid), len(grid[0])
+        grid[i][j] = 0;
+        curr[2] = Math.max(i, curr[2]);
+        curr[3] = Math.max(j, curr[3]);
+        for(int k = 0; k<4; k++) {
+            int x = dir[k] + i;
+            int y = dir[k + 1] + j;
+            dfs(grid, x, y, n, m, curr);
+        }
+    }
 
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == 1:
-                    curr = [0] * 4
-                    curr[0] = curr[2] = i
-                    curr[1] = curr[3] = j
-                    self.dfs(grid, i, j, n, m, curr)
-                    ans.append(curr)
-        
-        return ans
-        
+    public int[][] findFarmland(int[][] grid) {
+        List<int[]> resultList = new ArrayList<>();
+        int n = grid.length;
+        int m = grid[0].length;
+
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<m; j++) {
+                if(grid[i][j] == 1) {
+                    int[] curr = new int[4];
+                    curr[0] = curr[2] = i;
+                    curr[1] = curr[3] = j;
+                    dfs(grid, i, j, n, m, curr);
+                    resultList.add(curr);
+                }
+            }
+        }
+
+        int[][] ans = new int[resultList.size()][4];
+        for(int i = 0; i<resultList.size(); i++) {
+            ans[i] = resultList.get(i);
+        }
+
+        return ans;
+    }
+}
