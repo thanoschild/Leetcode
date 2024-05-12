f5bd14c6bed3836625f1df5df86a7e25129f03d3
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2024-05-12 21:51:51 +0530

    Time: 56 ms (30.89%), Space: 49.2 MB (27.36%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
@@ -11,15 +11,32 @@
  * @return {number[]}
  */
 var inorderTraversal = function(root) {
-    const ans = []
+    const ans = [];
+    if (!root) return ans;
 
-    inorder = (root) => {
-       if(root == null) return;
-       inorder(root.left);
-       ans.push(root.val)
-       inorder(root.right);
+    let prev = null;
+    let curr = root;
+
+    while (curr) {
+        if (!curr.left) {
+            ans.push(curr.val);
+            curr = curr.right;
+        } else {
+            prev = curr.left;
+            while (prev.right && prev.right !== curr) {
+                prev = prev.right;
+            }
+
+            if (!prev.right) {
+                prev.right = curr;
+                curr = curr.left;
+            } else {
+                prev.right = null;
+                ans.push(curr.val);
+                curr = curr.right;
+            }
+        }
     }
-    
-    inorder(root)
-    return ans
+
+    return ans;
 };
