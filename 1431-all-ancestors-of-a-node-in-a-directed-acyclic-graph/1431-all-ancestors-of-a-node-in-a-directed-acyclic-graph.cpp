5734daac158b2f5f98d7f74fbce6dcd9011c6966
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2024-06-29 11:48:15 +0530

    Time: 86 ms (97.15%), Space: 64.2 MB (87.49%) - LeetHub

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    void dfs(int i, vector<int> &curr, vector<bool> &visited, vector<vector<int>> &adj) {
-        visited[i] = true;
-        for(auto it : adj[i]) {
-            if(!visited[it]) {
-                curr.push_back(it);
-                dfs(it, curr, visited, adj);
+    void dfs(int x, int curr, vector<vector<int>>& ans, vector<vector<int>>& child) {
+        for(auto it : child[curr]) {
+            if(ans[it].size() == 0 || ans[it].back() != x) {
+                ans[it].push_back(x);
+                dfs(x, it, ans, child);
             }
         }
     }
-    
+
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n);
-        for(auto it : edges) {
-            adj[it[1]].push_back(it[0]);
+        vector<vector<int>> ans(n), child(n);
+        for (auto e : edges) {
+            child[e[0]].push_back(e[1]);
         }
 
-        vector<bool> visited(n, false);
-        vector<vector<int>> ans;
-        for(int i = 0; i<n; i++) {
-            vector<int> curr;
-            dfs(i, curr, visited, adj);
-            sort(curr.begin(), curr.end());
-            ans.push_back(curr);
-            fill(visited.begin(), visited.end(), false);
+        for (int i = 0; i < n; i++) {
+            dfs(i, i, ans, child);
         }
 
         return ans;
