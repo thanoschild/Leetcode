acb6936adf1c3ff402191a70192bb394a97624dd
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2024-07-10 19:20:50 +0530

    Time: 1 ms (99.42%), Space: 42.1 MB (37.73%) - LeetHub

---
+++ 1720-crawler-log-folder/1720-crawler-log-folder.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        int ans = 0;
+        for(String s : logs) {
+            if(s.equals("../")) ans = Math.max(0, --ans);
+            else if(s.equals("./")) continue;
+            else ans++;
+        }
+
+        return ans;
+    }
+}
