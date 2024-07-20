42a52d3344681cf37605ccfe8bf527da9144295b
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2024-07-20 17:20:47 +0530

    Time: 37 ms (60.27%), Space: 36.1 MB (11.51%) - LeetHub

---
+++ 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& row, vector<int>& col) {
+        int n = row.size(), m = col.size();
+        vector<vector<int>> ans(n, vector<int>(m, 0));
+        
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<m; j++) {
+                ans[i][j] = min(row[i], col[j]);
+                row[i] -= ans[i][j];
+                col[j] -= ans[i][j];
+            }
+        }
+
+        return ans;
+    }
+};
