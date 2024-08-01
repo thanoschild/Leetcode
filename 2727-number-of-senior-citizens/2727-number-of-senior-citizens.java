7ed7c79e9a56921b80ebf62c6b8851022839f3ae
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2024-08-01 22:19:49 +0530

    Time: 0 ms (100%), Space: 42.7 MB (34.64%) - LeetHub

---
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int ans = 0;
+        for(String s : details) {
+            int age = (s.charAt(11) - '0') * 10 + (s.charAt(12) - '0');
+            if(age > 60) ans++;
+        }
+
+        return ans;
+    }
+}
