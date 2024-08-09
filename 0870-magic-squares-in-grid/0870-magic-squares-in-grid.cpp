60e03e23f42ab2a31422080be5018b90e65b0227
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2024-08-09 11:07:48 +0530

    Time: 5 ms (32.35%), Space: 10.9 MB (59.66%) - LeetHub

---
+++ 0870-magic-squares-in-grid/0870-magic-squares-in-grid.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool checkGrid(int i, int j, vector<vector<int>>& grid) {
+        vector<int> counts(10, 0);
+
+        for (int k = i; k < i + 3; k++) {
+            int sumRows = 0;
+            for (int l = j; l < j + 3; l++) {
+                sumRows += grid[k][l];
+                if (grid[k][l] < 1 || grid[k][l] > 9) return false; 
+                counts[grid[k][l]]++;
+            }
+            if (sumRows != 15) return false;
+        }
+
+        for (int k = j; k < j + 3; k++) {
+            int sumCols = 0;
+            for (int l = i; l < i + 3; l++) {
+                sumCols += grid[l][k];
+            }
+            if (sumCols != 15) return false;
+        }
+
+        int leftd = 0, rightd = 0;
+        for (int k = 0; k < 3; k++) {
+            leftd += grid[i + k][j + k];
+            rightd += grid[i + k][j + 2 - k];
+        }
+
+        if (leftd != 15 || rightd != 15) return false;
+
+        for (int k = 1; k <= 9; k++) {
+            if (counts[k] != 1)
+                return false;
+        }
+
+        return true;
+    }
+
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        if (n < 3 || m < 3)
+            return false;
+        int ans = 0;
+
+        for (int i = 0; i <= n - 3; i++) {
+            for (int j = 0; j <= m - 3; j++) {
+                if (checkGrid(i, j, grid))
+                    ans++;
+            }
+        }
+
+        return ans;
+    }
+};
