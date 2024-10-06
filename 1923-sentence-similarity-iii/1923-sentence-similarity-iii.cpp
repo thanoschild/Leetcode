51e28f790f9ae592cb7e2af397aa9320087a667e
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2024-10-06 14:09:44 +0530

    Time: 2 ms (56.68%), Space: 8.9 MB (11.98%) - LeetHub

---
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool areSentencesSimilar(string s1, string s2) {
+        stringstream ss(s1);
+        string word;
+        vector<string> words1;
+        while(getline(ss, word, ' ')) {
+            words1.push_back(word);
+        }
+
+        stringstream sss(s2);
+        word = "";
+        vector<string> words2;
+        while(getline(sss, word, ' ')) {
+            words2.push_back(word);
+        }
+
+        int n = words1.size(), m = words2.size();
+        if(n > m) return areSentencesSimilar(s2, s1);
+
+        int i = 0;
+        while(i < n && words1[i] == words2[i]) i++;
+        while(i < n && words1[i] == words2[m - n + i]) i++;
+
+        return i == n;
+    }
+};
