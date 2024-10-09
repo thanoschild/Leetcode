ca3b8fd0a57b964d6373f07d6a47e41e71ccfe2e
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2024-10-09 17:35:14 +0530

    Time: 3 ms (42.94%), Space: 7.8 MB (81.46%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        stack<char> st;
-        int count = 0;
+        int open = 0, close = 0;
 
         for(char c : s) {
-            if(c == '(') st.push(c);
+            if(c == '(') open++;
             else {
-                if(!st.empty()) st.pop();
-                else count++;
+                if(open > 0) open--;
+                else close++;
             }
         }
 
-        return st.size() + count;
+        return open + close;
     }
 };
