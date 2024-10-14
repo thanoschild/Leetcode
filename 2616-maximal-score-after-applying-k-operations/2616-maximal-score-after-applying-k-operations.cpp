dde80ce4d093f3cff95690cc9932dc87dfefcb1d
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2024-10-14 23:03:05 +0530

    Time: 481 ms (5.24%), Space: 79.7 MB (50.62%) - LeetHub

---
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+        for(auto it : nums) pq.push(it);
+
+        long long ans = 0;
+        while(k-- && !pq.empty()) {
+            int t = pq.top();
+            cout<<t<<endl;
+            pq.pop();
+            ans += (long) t;
+            int val = t/3;
+            if(t%3 != 0) val++;
+            pq.push(val);
+        }
+
+        return ans;
+    }
+};
