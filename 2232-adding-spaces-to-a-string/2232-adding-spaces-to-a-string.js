7337e2a7fd752bb1f577376bc2def0566f9afe51
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2024-12-03 21:51:46 +0530

    Time: 37 ms (100%), Space: 81.9 MB (96.77%) - LeetHub

---
+++ 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {number[]} spaces
+ * @return {string}
+ */
+var addSpaces = function(s, spaces) {
+    let arr = [];
+    arr.push(s.slice(0, spaces[0]));
+    for(let i = 1; i<spaces.length; i++) {
+        arr.push(s.slice(spaces[i-1], spaces[i]));
+    }
+
+    arr.push(s.slice(spaces.at(-1)));
+    return arr.join(' ')
+};
