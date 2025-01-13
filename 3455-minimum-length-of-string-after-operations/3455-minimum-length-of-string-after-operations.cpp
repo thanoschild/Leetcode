e14fc19414ea62e639ec011ed118ef16f434d9d3
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2025-01-13 11:36:23 +0530

    Time: 12 ms (73.39%), Space: 32.1 MB (58.06%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-    int remChar(int n) {
-        while(n >= 3) {
-            n -= 2;
-        }
-        return n;
-    }
-
     int minimumLength(string s) {
         vector<int> freq(26, 0);
         for(char c : s) freq[c - 'a']++;
+
         int ans = 0;
         for(int i = 0; i<26; i++) {
-            ans += remChar(freq[i]);
+            if(freq[i] == 0) continue;
+            if(freq[i] % 2) ans += 1;
+            else ans += 2;
         }
 
         return ans;
