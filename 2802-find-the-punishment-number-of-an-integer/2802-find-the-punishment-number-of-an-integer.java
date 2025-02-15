752ee50550d7a5a8e6bb6694b96564fef551cc25
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2025-02-15 22:06:51 +0530

    Time: 5 ms (98.43%), Space: 40.4 MB (96.24%) - LeetHub

---
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    private boolean checkNum(int num, int target) {
+        if(target < 0 || num < target) return false;
+        if(num == target) return true;
+
+        return checkNum(num/10, target - num % 10) || checkNum(num/100, target - num % 100) || 
+        checkNum(num/1000, target - num % 1000);
+    }
+
+    public int punishmentNumber(int n) {
+        int ans = 0;
+        for(int i = 1; i<=n; i++) {
+            int num = i * i;
+            if(checkNum(num, i)) ans += num;
+        }
+
+        return ans;
+    }
+}
