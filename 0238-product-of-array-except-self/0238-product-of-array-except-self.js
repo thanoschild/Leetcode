1d82cf52d0cd433ffe6b2b37641ee312fd5a00ec
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2025-03-15 19:20:30 +0530

    Time: 7 ms (58.06%), Space: 69.3 MB (35.55%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -4,17 +4,18 @@
  */
 var productExceptSelf = function(nums) {
     const n = nums.length;
-    const ans = new Array(n);
-    ans[0] = 1;
+    const ans = new Array(n).fill(1);
 
-    for(let i = 1; i<n; i++) {
-        ans[i] = ans[i-1]*nums[i-1];
+    let curr = 1;
+    for(let i = 0; i<n; i++) {
+        ans[i] *= curr;
+        curr *= nums[i];
     }
 
-    let right = 1;
+    curr = 1;
     for(let i = n - 1; i>=0; i--) {
-        ans[i] *= right;
-        right *= nums[i];
+        ans[i] *= curr;
+        curr *= nums[i];
     }
 
     return ans;
