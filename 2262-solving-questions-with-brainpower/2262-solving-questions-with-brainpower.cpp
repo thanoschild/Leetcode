f5be9b32e0586e05d7da4791668a9e56dc05a3f6
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2025-04-01 19:57:33 +0530

    Time: 1462 ms (5.22%), Space: 147.6 MB (6.6%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -8,7 +8,7 @@ public:
         function<ll(int)> solve = [&](int idx) {
             if(idx >= n) return (ll) 0;
             if(dp[idx] != -1) return dp[idx];
-            ll ans1 = solve(qn[idx][1] + idx + 1) + 1L * qn[idx][0];
+            ll ans1 = solve(idx + qn[idx][1] + 1) + 1L * qn[idx][0];
             ll ans2 = solve(idx + 1);
             dp[idx] = max(ans1, ans2);
             return dp[idx];
