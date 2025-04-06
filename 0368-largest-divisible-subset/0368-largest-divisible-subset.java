1b3fa55298f88363f35e171997f92ad09fcf3c6d
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2025-04-06 21:36:34 +0530

    Time: 14 ms (66.53%), Space: 43.7 MB (14.77%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,34 +1,34 @@
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
         int n = nums.length;
-        if(n == 1) {
-            List<Integer> result = new ArrayList<>();
-            result.add(nums[0]);
-            return result;
-        }
+        if(n == 1) return Arrays.asList(nums[0]);
+        
+        int start = -1, maxVal = -1;
+        Arrays.sort(nums);
         int[] dp = new int[n];
         int[] parent = new int[n];
-        for(int i = 0; i<n; i++) dp[i] = 1;
+        Arrays.fill(dp, 1);
 
-        int source = 0, mx = 1;
-        Arrays.sort(nums);
         for(int i = 1; i<n; i++) {
             for(int j = 0; j<i; j++) {
-                if(nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1;
-                    parent[i] = j;
+                if(nums[i] % nums[j] == 0) {
+                    if(dp[i] < dp[j] + 1) {
+                        dp[i] = dp[j] + 1;
+                        parent[i] = j;
+                    }
                 }
             }
-            if(dp[i] > mx) {
-                mx = dp[i];
-                source = i;
+
+            if(dp[i] > maxVal) {
+                maxVal = dp[i];
+                start = i;
             }
         }
 
         List<Integer> ans = new ArrayList<>();
-        for(int i = 0; i<mx; i++) {
-            ans.add(nums[source]);
-            source = parent[source];
+        for(int i = 0; i<maxVal; i++) {
+            ans.add(nums[start]);
+            start = parent[start];
         }
 
         return ans;
