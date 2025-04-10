4573c1b7c0d9a88ad03193ea1f7acc65621c7c9b
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2025-04-11 01:39:03 +0530

    Time: 4 ms (32.26%), Space: 9.8 MB (65.59%) - LeetHub

---
+++ 3243-count-the-number-of-powerful-integers/3243-count-the-number-of-powerful-integers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long solve(string x, string s, int limit) {
+        if(x.length() < s.length()) return 0;
+        if(x.length() == s.length()) return x >= s ? 1 : 0;
+
+        string suffix = x.substr(x.length() - s.length(), s.length());
+        long long count = 0;
+        int preLen = x.length() - s.length();
+
+        for(int i = 0; i<preLen; i++) {
+            if(limit < (x[i] - '0')) {
+                count += (long) pow(limit + 1, preLen - i);
+                return count;
+            }
+            count += (long)(x[i] - '0') * (long) pow(limit + 1, preLen - 1 - i);
+        }
+
+        if(suffix >= s) count++;
+        return count;
+    }
+    
+    long long numberOfPowerfulInt(long long start, long long finish, int limit, string s) {
+        string start_ = to_string(start - 1), finish_ = to_string(finish);
+        return solve(finish_, s, limit) - solve(start_, s, limit);
+    }
+};
