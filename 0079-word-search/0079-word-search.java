a6a5b5ea682967432125463dc7e0339ac931dd32
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2025-08-09 00:50:28 +0530

    Time: 197 ms (23.57%), Space: 41.5 MB (89.32%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,30 +1,27 @@
 class Solution {
-    int[] dir = {-1, 0, 1, 0, -1};
+    int dir[] = {-1, 0, 1, 0, -1};
 
-    private boolean dfs(char[][] board, String word, int i, int j, int k) {
+    private boolean dfs(char[][] board, String word, int i, int j, int k, int n, int m) {
         if(k == word.length()) return true;
-        if(i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] != word.charAt(k)) {
-            return false;
-        }
+        if(i < 0 || i >= n || j < 0 || j >= m || board[i][j] != word.charAt(k)) return false;
 
         board[i][j] = '*';
         boolean flag = false;
-        for(int l = 0; l<4; l++) {
-            flag = flag || dfs(board, word, i + dir[l], j + dir[l + 1], k + 1);
+
+        for(int d = 0; d<4; d++) {
+            flag = flag || dfs(board, word, i + dir[d], j + dir[d+1], k+1, n, m);
         }
+
         board[i][j] = word.charAt(k);
         return flag;
     }
-    
+
     public boolean exist(char[][] board, String word) {
-        if(word.length() == 0) return false;
         int n = board.length, m = board[0].length;
 
         for(int i = 0; i<n; i++) {
             for(int j = 0; j<m; j++) {
-                if(board[i][j] == word.charAt(0) && dfs(board, word, i, j, 0)) {
-                    return true;
-                }
+                if(board[i][j] == word.charAt(0) && dfs(board, word, i, j, 0, n, m)) return true;
             }
         }
 
